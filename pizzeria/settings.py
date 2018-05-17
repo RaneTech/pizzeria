7cf3fdf94316c28e8459841a754d05f4ebf34ac1
zhouguanying <zhouguanying_qts@163.com>
2018-05-17 10:16:24 +0800

    Add 404 and 500 erro pages.

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'pizzeria.urls'
 TEMPLATES = [
 	{
 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-		'DIRS': [],
+		'DIRS': [os.path.join(BASE_DIR, 'pizzeria/templates')],
 		'APP_DIRS': True,
 		'OPTIONS': {
 			'context_processors': [
